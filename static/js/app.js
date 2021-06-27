e0ce1cc44fda54c3ae17a19c1c428a0754010184
FernB <fernbradder@gmail.com>
2021-06-27 15:06:29 +0800

    change null on gauge

--- static/js/app.js
+++ static/js/app.js
@@ -1,58 +1,58 @@
 
+// Initialise graphs
 
-
-var trace = {
+var databar = [{
     x: [],
     y: [],
     text:[],
     type: "bar",
     orientation: "h",
     hoverinfo:"text",
-    };
+    }];
     
-var databar = [trace];
+// var databar = [trace];
 
 var layoutbar = {
   hovermode: 'closest',
   hoverlabel: { bgcolor: "#4eb3d3", opacity:0.4},
   title: 'Top 10 Occuring Microbial Species'
-};
+  };
     
-    Plotly.newPlot("bar",databar, layoutbar)
+Plotly.newPlot("bar",databar, layoutbar)
 
 
 
-    var trace1 = {
-        x: [1, 2, 3, 4],
-        y: [10, 11, 12, 13],
-        text: [],
-        hoverinfo: "text",
-        mode: 'markers',
-        marker: {
-          color: [10, 11, 12, 13],
-          colorscale: 'Cividis',
-          opacity: [1, 0.8, 0.6, 0.4],
-          size: [40, 60, 80, 100],
+var databubble = [{
+    x: [],
+    y: [],
+    text: [],
+    hoverinfo: "text",
+    mode: 'markers',
+    marker: {
+        color: [],
+        colorscale: 'Cividis',
+        opacity: 0.8,
+        size: [],
         }
-      };
+    }];
       
-      var data3 = [trace1];
+     
       
-      var layoutbubble = {
-        title: 'All Microbial Species',
-        hovermode: 'closest',
-        showlegend: false,
-        height: 800,
-        width: 1200,
-        xaxis: {title: {
-          text: 'OTU ID'
+var layoutbubble = {
+    title: 'All Microbial Species',
+    hovermode: 'closest',
+    showlegend: false,
+    height: 800,
+    width: 1200,
+    xaxis: {title: {
+        text: 'OTU ID'
         }}
-      };
+    };
       
-      Plotly.newPlot('bubble', data3, layoutbubble);
+Plotly.newPlot('bubble', databubble, layoutbubble);
 
-
-      var data = [
+// Gauge as pie chart for greater control over needle
+var datagauge = [
         // {
         //   domain: { x: [0, 1], y: [0, 1] },
         //   // value: 2,
@@ -97,19 +97,20 @@ var layoutbar = {
           hole: .4,
           type: 'pie'
 
-        }
-
-
-
-
-      ];
+        }];
       
-      var layout = { 
+      var layoutgauge = { 
         width: 400, 
         height: 400, 
         margin: { t: 0, b: 0 },
-        title: {text:'<b>Bellybutton Washing Frequency</b>', x:0.5, xanchor:'center', y:0.98, yanchor:'top'},
-        annotations: [{text:'Scrubs per Week', x:0.5, xanchor:'center', y:0.9, yanchor:'top', showarrow: false}],
+        title: {text:'<b>Bellybutton Washing Frequency</b>', 
+                x:0.5, xanchor:'center', 
+                y:0.98, yanchor:'top'
+                },
+        annotations: [{text:'Scrubs per Week', 
+                      x:0.5, xanchor:'center', 
+                      y:0.9, yanchor:'top', 
+                      showarrow: false}],
         showlegend: false,
         shapes: [
           {
@@ -137,167 +138,115 @@ var layoutbar = {
             line: {
               color: 'firebrick'
             }},
+        ]};
 
-    
-
-
-        ]
-
-      };
-
-      Plotly.newPlot('gauge', data, layout);
+Plotly.newPlot('gauge', datagauge, layoutgauge);
 
 
-
-
-
+// Get json data
 
 d3.json("samples.json").then(data =>{
-    console.log(data.samples);
-
-
-
+    
+    // create dropdown list from subjects
     var subjects = data.names;
     var subjectselect = d3.select("#selDataset");
                 
     subjects.forEach(i => {
-    var option = subjectselect.append("option");                 
-    var optionValue = option.attr("value",i);
-    optionValue.text(i);
-    });
+      var option = subjectselect.append("option");                 
+      var optionValue = option.attr("value",i);
+      optionValue.text(i);
+      });
 
+    // default is first subject  
     var subject = "940";
 
-
+    // call function to update plot for default
     updatePlot(subject);
 
 
 
-
+    // event listener to filter data on selection of new subject from dropdown
     d3.selectAll("#selDataset").on("change", filterData);
 
+    // filter function
     function filterData(){
 
+      // set subject as filter option
       var filterOption = d3.select("#selDataset");
   
       subject = filterOption.property("value");
 
+      // call update plot function
       updatePlot(subject);
 
-    }
+    };
 
+// function to update plot with new subject values
+    function updatePlot(subject) {
 
-function updatePlot(subject) {
+      // get subject metadata
+        var metadatapanel = d3.select('#sample-metadata');
+        metadatapanel.selectAll("p").remove();
+        var metadataid = data.metadata.filter(d => d.id === parseInt(subject));
+        
+        // get wash frequecy for gauge 
+        var washes = metadataid[0].wfreq;
+        
 
+        // var rwash = (washes == 0 | washes == null) ? 3 : (((18*(10-washes))+(washes-5))*(Math.PI/180));
+        // calculate radians to turn indicator line
+        var rwash = (washes == 0 | washes == null) ? 3.1 : (202.5+(-22.5*washes))*(Math.PI/180);
 
-  var metadatapanel = d3.select('#sample-metadata');
-  metadatapanel.selectAll("p").remove();
-  var metadataid = data.metadata.filter(d => d.id === parseInt(subject));
-  var washes = metadataid[0].wfreq;
-  console.log(washes)
+        // get (x,y) for line end 
+        var xwash = 0.2*Math.cos(rwash)+0.5;
+        var ywash = 0.2*Math.sin(rwash)+0.5;
+       
 
-  // var rwash = (washes == 0 | washes == null) ? 3 : (((18*(10-washes))+(washes-5))*(Math.PI/180));
+        // update panel with metadata
+        Object.entries(metadataid[0]).forEach(([key,value]) => {
+          metadatapanel.append("p").text(key + ": " + value);
+          });
 
-  var rwash = (washes == 0 | washes == null) ? 2.5 : (202.5+(-22.5*washes))*(Math.PI/180);
-
-
-  var xwash = 0.2*Math.cos(rwash)+0.5;
-  var ywash = 0.2*Math.sin(rwash)+0.5;
-  console.log(xwash)
-
-  
-  Object.entries(metadataid[0]).forEach(([key,value]) => {
-  
-  metadatapanel.append("p").text(key + ": " + value);
-   
-    });
-
-
-    var e = data.samples.filter(d => d.id === subject);
-// e.sort((a,b) => b.sample_values[0] - a.sample_values[0]);
-    // console.log(e[0]);
-// var x = e[0];
-
-    var sample_values = e.map(d => d.sample_values);
-
-
-
-// console.log(sample_values[0]);
-// var indexList = sample_values[0].map((item, index, array) => {return {"index": index, "item": item}});
-// console.log(indexList)
-// var sortList = indexList.sort((a,b) => b.item - a.item);
-//  var sortList = indexList.map(b => b.index).slice(0,10).reverse();
-
-// var top10 = e.slice(0,10).reverse();
-// var resultlist = []
-
-// var result = {};
-// var keys = ['otu_ids',"otu_labels","sample_values"];
-var otu_ids = e.map(d => d.otu_ids);
-// keys.forEach((i) => result[] = 
-var otu_labels = e.map(d => d.otu_labels);
-
-// var sortedValues = sortList.map(i => sample_values[0][sortList[i]])
-// var sortedIDs = sortList.map(i => otu_ids[i])
-// var sortedLabels = sortList.map(i => otu_labels[i])
-
-var top10values = sample_values[0].slice(0,10);
-var top10ids = otu_ids[0].slice(0,10);
-var idlb = top10ids.map(d => "otu " + d);
-var top10labels = otu_labels[0].slice(0,10);
-var labelsplit = top10labels.map(d => (d.split(";")).join("<br>"));
-console.log(labelsplit)
-var alllabels = otu_labels[0].map(d => (d.split(";")).join("<br>"));
-
-
-
-
-
-
-// var max = top10ids.reduce((a,b) => Math.max(a,b));
-// var min = top10ids.reduce((a,b) => Math.min(a,b));
-
-Plotly.restyle("bar", 'x', [top10values.reverse()]);
-Plotly.restyle('bar','y',[idlb.reverse()]);
-Plotly.restyle('bar','text',[labelsplit.reverse()]);
-Plotly.restyle("bubble", 'y', [sample_values[0]]);
-Plotly.restyle("bubble", 'x', [otu_ids[0]]);
-Plotly.restyle('bubble',{'marker.size':[sample_values[0]]});
-Plotly.restyle('bubble',{'marker.color':[otu_ids[0]]});
-Plotly.restyle('bubble','text',[alllabels]);
-Plotly.relayout('gauge',{'shapes[0].x1':xwash,'shapes[0].y1':ywash})
-
-
-
-
-// var metadatapanel = d3.select('#sample-metadata');
-// var metadataid = data.metadata.filter(d => d.id === parseInt(subject));
-
-// Object.entries(metadataid[0]).forEach(([key,value]) => {
-
-// metadatapanel.append("p").text(key + ": " + value);
+          // get sample data for subect
+        var e = data.samples.filter(d => d.id === subject);
  
-//   });
+        // map sample values
+        var sample_values = e.map(d => d.sample_values);
+
+        // map otu ids
+        var otu_ids = e.map(d => d.otu_ids);
+
+        // map otu lables 
+        var otu_labels = e.map(d => d.otu_labels);
+
+      // var sortedValues = sortList.map(i => sample_values[0][sortList[i]])
+      // var sortedIDs = sortList.map(i => otu_ids[i])
+      // var sortedLabels = sortList.map(i => otu_labels[i])
+      
+      // slice to get top 10 values for bar chart
+      var top10values = sample_values[0].slice(0,10);
+      var top10ids = otu_ids[0].slice(0,10);
+      var idlb = top10ids.map(d => "otu " + d);
+      var top10labels = otu_labels[0].slice(0,10);
+
+      // make lables appear with line breaks so easier to read
+      var labelsplit = top10labels.map(d => (d.split(";")).join("<br>"));
+      
+      // get all labels for bubble chart
+      var alllabels = otu_labels[0].map(d => (d.split(";")).join("<br>"));
 
 
 
-
-
-
-// var trace = {
-//     x: top10values.reverse(),
-//     y: idlb.reverse(),
-//     type: "bar",
-//     orientation: "h"
-//     };
-    
-//     var data2 = [trace];
-    
-//     Plotly.newPlot("bar",data2)
-
-
-
-
+      // restyle all plots
+      Plotly.restyle("bar", 'x', [top10values.reverse()]);
+      Plotly.restyle('bar','y',[idlb.reverse()]);
+      Plotly.restyle('bar','text',[labelsplit.reverse()]);
+      Plotly.restyle("bubble", 'y', [sample_values[0]]);
+      Plotly.restyle("bubble", 'x', [otu_ids[0]]);
+      Plotly.restyle('bubble',{'marker.size':[sample_values[0]]});
+      Plotly.restyle('bubble',{'marker.color':[otu_ids[0]]});
+      Plotly.restyle('bubble','text',[alllabels]);
+      Plotly.relayout('gauge',{'shapes[0].x1':xwash,'shapes[0].y1':ywash})
 
 
 }
